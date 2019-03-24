139147b4a6488e059a6779d5ea0b880468ccc622
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-24 20:33:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,7 +23,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     if holiday == :fourth_of_july
       return holiday[2]
     end
-  
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
